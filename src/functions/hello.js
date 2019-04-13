5f056188e0c0ff3587d08b2b18f364b6f2898d57
Ruben Meseguer <rubenmeseguer7@gmail.com>
2019-04-13 21:13:24 +0200

    send test emails working

--- src/functions/hello.js
+++ src/functions/hello.js
@@ -1,8 +1,47 @@
-exports.handler = async (event, context) => {
-  const name = event.queryStringParameters.name || "World";
+import fetch from 'node-fetch';
 
-  return {
-    statusCode: 200,
-    body: `Hello, ${name}`
-  };
+const nodemailer = require('nodemailer');
+
+
+exports.handler = async function (event, context, callback) {
+  try {
+    const transporter = nodemailer.createTransport({
+      host: 'smtp.ethereal.email',
+      port: 587,
+      secure: false, // true for 465, false for other ports
+      auth: {
+        user: 'tod48@ethereal.email', // generated ethereal user
+        pass: 'MZtBHX5Z6WJQ12jktK', // generated ethereal password
+      },
+    });
+    const mailOptions = {
+      from: 'foo@example.com',
+      to: 'jinamus@getnada.com',
+      subject: 'Freelancing Request',
+      text: 'That was easy!',
+    };
+
+    const response = await transporter.sendMail(mailOptions);
+    return {
+      statusCode: 201,
+      body: `We waited ${JSON.stringify(response)}`,
+    };
+  } catch (e) {
+    return {
+      statusCode: 201,
+      body: e,
+    };
+  }
 };
+
+/*
+* (error, info) => {
+    if (error) {
+
+    }
+    return {
+      statusCode: 201,
+      body: 'Email sent',
+    };
+*
+* */
